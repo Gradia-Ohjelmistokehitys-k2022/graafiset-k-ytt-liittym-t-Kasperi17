8b2b1f29c7dc2d011132e7dda509e531f0eaa4f9
Kasperi Salonen <gr275829@gradia.fi>
2023-10-03 17:55:25 +0300

    tehtävä

---
+++ 05_For/Tehtävä 5/Form1.cs
@@ -0,0 +1,26 @@
+using System.Windows.Forms;
+
+namespace Tehtävä_5
+{
+    public partial class Pääformi : Form
+    {
+        public Pääformi()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= Int32.Parse(textBox1.Text); i++)
+            {
+                label1.Text = i.ToString();
+                Thread.Sleep(100);
+                label1.Update();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
